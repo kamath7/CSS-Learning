4cee3964e3d42996d1d28cd8fd86ba6fbef71412
kamath7 <adithyakamath96@gmail.com>
2021-04-08 03:53:17 +0530

    Transitions complete

--- Transitions and Animation/Project-Earlier/shared.js
+++ Transitions and Animation/Project-Earlier/shared.js
@@ -4,7 +4,7 @@ var modalNoButton = document.querySelector(".modal__action--negative");
 var selectPlanButtons = document.querySelectorAll(".plan button");
 var toggleButton = document.querySelector(".toggle-button");
 var mobileNav = document.querySelector(".mobile-nav");
-
+var ctaButton = document.querySelector('.main-nav__item--cta')
 // console.dir(backdrop.style['background-image']);
 
 // console.dir(backdrop);
@@ -53,3 +53,13 @@ toggleButton.addEventListener("click", function() {
   },10)
 
 });
+
+ctaButton.addEventListener('animationstart',function(event){
+  console.log('Animation started')
+})
+ctaButton.addEventListener('animationend',function(event){
+  console.log('Animation Ended')
+})
+ctaButton.addEventListener('animationiteration',function(event){
+  console.log('Animation Iteration')
+})
